38750498ce0caf58c0e22353a6b5d84f0a99a67f
Aravinthan333 <aravinthan2606@gmail.com>
2024-09-12 02:14:35 +0530

    "Updated ESLint config and refactored App.jsx component"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,54 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import FloatingShape from "./components/FloatingShape";
+
+import DashboardPage from "./pages/DashboardPage";
+import SignUpPage from "./pages/SignUpPage";
+import EmailVerificationPage from "./pages/EmailVerificationPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import ForgotPasswordPage from "./pages/ForgotPassword";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
   return (
     <>
-      <div className="text-rose-600">APP</div>
+      <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+        <FloatingShape
+          color="bg-green-500"
+          size="w-64 h-64"
+          top="-5%"
+          left="10%"
+          delay={0}
+        />
+
+        <FloatingShape
+          color="bg-emerald-500"
+          size="w-48 h-48"
+          top="70%"
+          left="80%"
+          delay={5}
+        />
+
+        <FloatingShape
+          color="bg-lime-500"
+          size="w-32 h-32"
+          top="40%"
+          left="-10%"
+          delay={2}
+        />
+
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/" element={<SignUpPage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/" element={<ForgotPasswordPage />} />
+          <Route path="/" element={<ResetPasswordPage />} />
+          <Route path="/" element={<EmailVerificationPage />} />
+        </Routes>
+
+        <Toaster />
+      </div>
     </>
   );
 }
